d410b85338c904454aa039b8a16a6d67f16a520c
monika12b <monika12b@gmail.com>
2021-08-18 19:17:19 -0700

    updating reset, winner and loser logics

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const p1Btn = document.querySelector("#p1Btn");
 const p2Btn = document.querySelector("#p2Btn");
-const reset = document.querySelector("#reset");
+const resetBtn = document.querySelector("#resetBtn");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const winningScoreSelect = document.querySelector("#playTo");
 
 let p1Score = 0;
 let p2Score = 0;
@@ -14,6 +15,8 @@ p1Btn.addEventListener("click", function () {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
     }
     p1Display.textContent = p1Score;
   }
@@ -24,15 +27,26 @@ p2Btn.addEventListener("click", function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("loser");
+      p2Display.classList.add("winner");
     }
     p2Display.textContent = p2Score;
   }
 });
 
-reset.addEventListener("click", function () {
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+resetBtn.addEventListener("click", reset);
+
+function reset() {
   isGameOver = false;
   p1Score = 0;
   p2Score = 0;
   p1Display.textContent = 0;
   p2Display.textContent = 0;
-});
+  p1Display.classList.remove('winner', 'loser');
+  p2Display.classList.remove('winner', 'loser');
+}
